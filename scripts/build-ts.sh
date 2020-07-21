7fe899c9b2b63e4b1254dc97fac5e6ee569b502b
linhnt <linhnt@fabbi.com.vn>
2020-07-21 19:59:27 +0700

    add build script

--- scripts/build-ts.sh
+++ scripts/build-ts.sh
@@ -2,12 +2,10 @@
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 cd $dir
 cd ../public
-find . ! -name 'index.php' -type f -exec rm -rf {} +
-rm -r css img js
-ls -la
-cd ../frontend/vuejs
+find . ! -name 'index.php' -type f -exec rm -f {} +
+cd ../frontend/vuejs-ts
 npm run build
 cd ../../public
 mv dist/* ../public/
 rm -rf dist
-#mv index.html spa.blade.php
+mv index.html spa.blade.php
