d327d6cbf02b6120fe75ee5f304158d9f2f5420e
linhnt <linhnt@fabbi.com.vn>
2020-09-01 11:36:40 +0700

    php-lint

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,8 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         //
+        \App\Console\Commands\PrecommitHook::class,
+        \App\Console\Commands\InstallGitHooks::class,
     ];
 
     /**
